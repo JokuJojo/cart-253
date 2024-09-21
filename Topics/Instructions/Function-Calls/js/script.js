b27af4b46e69c2ec9935a0d56e1ab0711d21616f
JokuJojo <Joranemilot@gmail.com>
2024-09-21 16:24:45 -0400

    Added comments to the Blank page comments

--- Topics/Instructions/Function-Calls/js/script.js
+++ Topics/Instructions/Function-Calls/js/script.js
@@ -1,22 +1,32 @@
 /**
- * Function Calls
+ * The Blank Page
  * Joku Jojo
  * 
- * Working on Function calls examples.
+ * An exploration of the existential angst of a novelist
+ * who must sit down at their pink desk and confront
+ * the abyss that is a blank page of paper.
+ * 
+ * The program is non-interactive to convey the
+ * inability to get started on the project.
+ * Try typing! Just try it!
  */
 
 "use strict";
 
-
+/**
+ * Created the canvas for our masterpiece
+ */
 function setup() {
-    //once at the beginning of the program
+    //Create the canvas at a standard reslution
     createCanvas(640, 480);
 
 }
 
-
+/**
+ * Draws the writer's desktop and a blank peice of paper
+ */
 function draw() {
-    //Every frame
+    //The pink desktop
     background(255, 100, 100);
     rect(200, 80, 240, 320)
 }
