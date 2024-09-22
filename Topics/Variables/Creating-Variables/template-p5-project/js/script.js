3bf193091629cfd45f61b04b615ea0d074e131b0
JokuJojo <Joranemilot@gmail.com>
2024-09-22 15:13:49 -0400

    transformed the cheese-hole drawing program with variables!

--- Topics/Variables/Creating-Variables/template-p5-project/js/script.js
+++ Topics/Variables/Creating-Variables/template-p5-project/js/script.js
@@ -6,6 +6,15 @@
 
 "use strict";
 
+let cheeseRed = 255;
+let cheeseGren = 255;
+let cheeseBlue = 0;
+
+let holeShade = 0;
+let holeSize = 120;
+let holeX = 140;
+let holSize = 120;
+
 /**
 create the canvas*/
 function setup() {
@@ -18,12 +27,12 @@ draws a hole in a peice of cheese*/
 function draw() {
 
     // the cheese
-    background(255, 255, 0);
+    background(cheeseRed, cheeseGreen, cheeseBlue);
 
     //teh hole
     push();
     noStroke();
     fill(0);
-    ellipse(140, 175, 180);
+    ellipse(140, 175, holesize);
     pop();
 }
