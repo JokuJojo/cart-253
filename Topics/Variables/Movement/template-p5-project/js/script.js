17d847ca691a4d40efed3dd89f70aad45f7159dd
JokuJojo <Joranemilot@gmail.com>
2024-09-22 16:25:50 -0400

    Bird flew very fast veolicty ectttt
    im very tired

--- Topics/Variables/Movement/template-p5-project/js/script.js
+++ Topics/Variables/Movement/template-p5-project/js/script.js
@@ -1,5 +1,5 @@
 /**
- * Title of Project
+movement
  * Author Name
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
@@ -8,17 +8,32 @@
 
 "use strict";
 
+let bird = {
+    x: 120,
+    y: 480,
+    size:50
+}
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
+    createCanvas(640, 480);
 
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * move bird
 */
 function draw() {
+    background(0);
 
+    //move bird
+
+    //draw the bird
+    push();
+    fill(255, 0, 0);
+    noStroke();
+    ellipse(bird.x, bird.y, bird.size);
+    Pop();
 }
