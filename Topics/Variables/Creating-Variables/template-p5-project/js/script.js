afcfc11897635cf720f7008a94b1b01040add72c
JokuJojo <Joranemilot@gmail.com>
2024-09-22 14:58:29 -0400

    starte the creating variables project

---
+++ Topics/Variables/Creating-Variables/template-p5-project/js/script.js
@@ -0,0 +1,22 @@
+/**
+ * Creating Vriables
+ * Author Name
+ * 
+ * Creating variables */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
